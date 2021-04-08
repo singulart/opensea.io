9332ce0067d64625ec6f17aca995623e021da20d
singulart <serpian@gmail.com>
2021-04-08 13:08:48 +0300

    remove unnecessary scripts

--- cryptopunks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-step=250
-event="created"
-for i in $(seq 0 $step 10000);
-do
-  export scrape_url=$(printf "https://api.opensea.io/api/v1/events?collection_slug=cryptopunks&only_opensea=false&event_type=$event&offset=$i&limit=$step" "$i");
-  echo $scrape_url;
-  curl --request GET --url $scrape_url > "$event"_$i.json;
-done
